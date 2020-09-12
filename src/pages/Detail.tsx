5e15be5d1b7ab72be89bd68a2011cfd65a6a55ad
Max Kim <hwaseen@gmail.com>
2020-09-12 16:41:01 +0900

    setting:router, page setting

---
+++ src/pages/Detail.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Detail() {
+    return (
+        <div className="detail">
+            디테일
+        </div>
+    );
+}
+
+export default Detail
---
+++ src/routes/index.tsx
@@ -0,0 +1,22 @@
+import React, {Suspense} from 'react';
+import {BrowserRouter, Route, Switch} from 'react-router-dom'
+
+const HomePage = React.lazy(() => import('../pages/Home'))
+const DetailPage = React.lazy(() => import('../pages/Detail'))
+const NotFoundPage = React.lazy(() => import('../pages/NotFound'))
+
+function RootRouter() {
+    return (
+        <BrowserRouter>
+            <Suspense fallback={<div>로딩</div>}>
+                <Switch>
+                    <Route exact path="/" component={HomePage} />
+                    <Route path="/detail/:id" component={DetailPage} />
+                    <Route path="*" component={NotFoundPage} />
+                </Switch>
+            </Suspense>
+        </BrowserRouter>
+    )
+}
+
+export default RootRouter;
