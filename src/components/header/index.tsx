6d57dd22ae9b449b8228c4ecbe9f56bd0e43d6bf
Max Kim <hwaseen@gmail.com>
2020-09-17 23:01:01 +0900

    feat:add home btn link in header component

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -4,9 +4,9 @@ import './_header.scss'
 function Header() {
   return (
     <header>
-      <div className="header-title">
-        판례요지봇
-      </div>
+      <a href="/">
+        <div className="header-title">판례요지봇</div>
+      </a>
     </header>
   )
 }
