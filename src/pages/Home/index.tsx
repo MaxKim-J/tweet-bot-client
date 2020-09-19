30976c2ef47b3f8313acc172d794079b16504f50
Max Kim <hwaseen@gmail.com>
2020-09-19 13:55:34 +0900

    fix:remove elem flicking when redering

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -8,20 +8,24 @@ import { flattenPrevTweetList } from '../../utils/flattenHelper'
 function HomePage() {
   const [prevTweetList, setPrevTweetList] = useState([])
   const [uploadedTweetCount, setUploadedTweetCount] = useState(0)
+  const [isShowContent, setIsShowContent] = useState(false)
 
   useEffect(() => {
     const fetchPrevTweetList = async (last:number) => {
       const { data: { counts, tweets } } = await httpRequest.getPreviousUploadedTweets(last)
       setPrevTweetList(flattenPrevTweetList(tweets))
       setUploadedTweetCount(counts)
+      setIsShowContent(true)
     }
     fetchPrevTweetList(10)
   }, [])
   return (
-    <div className="home">
+    <>
+      {isShowContent && <div className="home">
         <AppIntroduce uploadedTweetCount={uploadedTweetCount} />
         <List prevTweetList={prevTweetList} />
-    </div>
+      </div>}
+    </>
   )
 }
 
